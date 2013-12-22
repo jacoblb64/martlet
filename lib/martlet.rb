dff59820077834aa3c21ffb144a50e2008daf0cc
Alex Coco <alex.coco@shopify.com>
2013-12-22 18:04:31 -0500

    Allow client to fetch grades

--- lib/martlet.rb
+++ lib/martlet.rb
@@ -1,5 +1,8 @@
 require 'martlet/client'
 require 'martlet/authenticator'
+require 'martlet/transcript'
+require 'martlet/transcript_parser'
+require 'martlet/record'
 require 'martlet/version'
 
 module Martlet
--- lib/martlet/client.rb
+++ lib/martlet/client.rb
@@ -11,5 +11,10 @@ module Martlet
       authenticator = Authenticator.new(@agent)
       authenticator.authenticate(email, password)
     end
+    
+    def grades
+      transcript = Transcript.new(@agent)
+      transcript.fetch_grades
+    end
   end
 end
