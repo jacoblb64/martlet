7292f726d29abe450c12a21db66e0d91cd719d1f
Alex Coco <alex.coco@shopify.com>
2013-12-22 18:23:31 -0500

    Allow just the name to be passed as the email

--- lib/martlet/client.rb
+++ lib/martlet/client.rb
@@ -6,15 +6,25 @@ module Martlet
     
     def initialize(email, password)
       @agent = Mechanize.new
-      @email = email
+      @email = format_email(email)
       
       authenticator = Authenticator.new(@agent)
-      authenticator.authenticate(email, password)
+      authenticator.authenticate(@email, password)
     end
     
     def grades
       transcript = Transcript.new(@agent)
       transcript.fetch_grades
+    end
+    
+    private
+    
+    def format_email(email)
+      if email.include?('@')
+        email
+      else
+        "#{email}@mail.mcgill.ca"
+      end
     end
   end
 end
