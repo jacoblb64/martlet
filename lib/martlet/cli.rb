b66e643bfc2e68c34ee92ad1c844280f5b545673
Alex Coco <alex.coco@shopify.com>
2014-04-17 23:36:13 -0400

    Use sort instead of order by

--- lib/martlet/cli.rb
+++ lib/martlet/cli.rb
@@ -7,12 +7,12 @@ module Martlet
     default_task :grades
 
     desc "grades", "Lists all your grades"
-    method_option :order_by, aliases: 'o', enum: ['course', 'grade']
+    method_option :sort, aliases: 's', enum: ['course', 'grade']
     def grades
       puts 'Fetching grades...'
       grades = client.grades
 
-      grades = case options.order_by
+      grades = case options['sort']
       when 'course' then grades.sort_by { |k,v| k }
       when 'grade'  then grades.sort_by { |k,v| Grade.new v }
       else grades
