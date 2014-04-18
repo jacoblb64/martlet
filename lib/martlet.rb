f15d9d7213ad00444939cf059ce88c95e6c29d53
Alex Coco <alex.coco@shopify.com>
2014-04-17 23:20:22 -0400

    Require all the things

--- lib/martlet.rb
+++ lib/martlet.rb
@@ -1,8 +1,13 @@
 require 'martlet/client'
 require 'martlet/authenticator'
+require 'martlet/day_conversions'
 require 'martlet/transcript'
 require 'martlet/transcript_parser'
+require 'martlet/schedule'
+require 'martlet/schedule_parser'
 require 'martlet/record'
+require 'martlet/grade'
+require 'martlet/course'
 require 'martlet/version'
 
 module Martlet
