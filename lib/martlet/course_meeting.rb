597ea769b965b34ee06d22bc570ec42ae0e703f6
quelledanielle <quelledanielle@gmail.com>
2014-08-13 16:07:52 -0400

    Add course meeting initialize and parsing methods

--- lib/martlet/course_meeting.rb
+++ lib/martlet/course_meeting.rb
@@ -1,4 +1,29 @@
 module Martlet
   class CourseMeeting
+    include DayConversions
+
+    attr_reader :start_time, :end_time, :start_date, :end_date, :days, :location
+
+    def initialize(args)
+      @start_time, @end_time = parse_time_range(args[:time])
+      @start_date, @end_date = parse_date_range(args[:date_range])
+      @days     = parse_days(args[:days])
+      @location = args[:location]
+    end
+
+    private
+
+    def parse_time_range(time_range)
+      time_range.scan(/\d+:\d+\s[AP]M/)
+    end
+
+    def parse_date_range(date_range)
+      dates = date_range.scan(/\w+\s\d+,\s\d+/)
+      dates.map { |date| Date.parse(date) }
+    end
+
+    def parse_days(days)
+      days.split('').map { |letter| day_from_letter(letter) }
+    end
   end
 end
